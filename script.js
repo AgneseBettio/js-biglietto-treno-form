3218218659101da8868c147ca78679c977b44747
AgneseBettio <bettio.agnese@gmail.com>
2021-04-17 18:21:04 +0200

    ok ma ora curo stile css

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 // ricopiato tutto js vecchio esercizio
-
-
 /*il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65 */
@@ -15,6 +13,16 @@ va applicato uno sconto del 40% per gli over 65 */
 // console.log("biglietto scontato Over", prezzoScontoOver);
 // var prezzoScontoJunior = (prezzoIntero - ((prezzoIntero / 100) * 20)).toFixed(2);
 // console.log("biglietto scontato Junior", prezzoScontoJunior);
+
+// Partendo dall'esercizio del biglietto, aggiungiamo la parte visiva come da screenshot
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza
+// Prezzo calcolato
+// Categoria selezionata dall'utente
+// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+
 var ticketForm = document.getElementById("ticket-form");
 ticketForm.addEventListener("submit", function (event) {
     //blocco refresh pagina default
@@ -34,43 +42,40 @@ ticketForm.addEventListener("submit", function (event) {
     var nCarrozzaStampato = document.getElementById("carrozza");
     var codidiceBigliettoStampato = document.getElementById("codice");
     var costoBigliettoStampato = document.getElementById("costo");
-
+    // altre variabili per stampa
+    var bigliettoDefault = (0.21 * parseInt(kmUtente)).toFixed(2);
+    var bigliettoScontoMin = (bigliettoDefault - ((bigliettoDefault / 100) * 20)).toFixed(2);
+    var bigliettoScontoOver = (bigliettoDefault - ((bigliettoDefault / 100) * 40)).toFixed(2);
 
     // preparo per stampa valori in HTML
     nomeUtenteStampato.innerHTML = nomeUtente;
-    // //funzione calcolo prezzo
-    // offertaStampato.innerHTML =
+    //funzione calcolo prezzo
+    if (formElements.eta.value === "minorenne") {
+        offertaStampato.innerHTML = " sconto minorenne ";
+    } else if (formElements.eta.value === "over65") {
+        offertaStampato.innerHTML = " sconto over 65 ";
+    } else {
+        offertaStampato.innerHTML = " non applicabile "
+    }
     //funzione random carrozza
     nCarrozzaStampato.innerHTML = randomNumberMax12();
     //funzione random ID codice biglietto
     codidiceBigliettoStampato.innerHTML = getRandomNumberBetween(10000, 99999);
     //funzione calcolo prezzo km e eta
-    costoBigliettoStampato.innerHTML = function calcoloBiglietto(eta, kmUtente);
+    if (formElements.eta.value === "over65") {
+        costoBigliettoStampato.innerHTML = bigliettoScontoOver + "€";
+    } else if (formElements.eta.value === "minorenne") {
+        costoBigliettoStampato.innerHTML = bigliettoScontoMin + "€";
+    } else {
+        costoBigliettoStampato.innerHTML = bigliettoDefault + "€";
+    }    
+    showIt("printed-ticket");
 })
 
 //se clicchiamo su annulla dobbiamo ripulire il form.
-ticketForm.addEventListener("reset", function () {
-
+ticketForm.addEventListener("reset", function (event) {
+    document.getElementById("printed-ticket").style.display = "none";
 })
-
-
-//var prezzoDefault = (0.21 * km).toFixed(2);
-
-//prezzo del biglietto stampato dipenderà da funzione:
-
-function calcoloBiglietto(eta, km) {
-
-    var costo = (0.21 * km).toFixed(2);
-
-    if (eta < 18) {
-        costo = (costo - ((costo / 100) * 20)).toFixed(2);
-        return costo;
-
-    } else if (eta >= 65) {
-        costo = (costo - ((costo / 100) * 40)).toFixed(2);
-        return costo;
-    }
-}
 //funzione per numeri random sino a 12
 function randomNumberMax12() {
     return Math.ceil(Math.random() * 12);
@@ -80,30 +85,14 @@ function getRandomNumberBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function showIt(id) {
+    document.getElementById(id).style.display = 'block';
+}
+
 
 
 
 // if (kmPercorsi === "" || etaPasseggero === "") {
 //     alert("manca campo obligatorio")
-// } else if (Number.isNaN(kmPercorsi) || Number.isNaN(etaPasseggero)) {
-//     alert ("i km da percorrere e la vostra età vanno inseriti in cifre")    
-// } else { 
-//     var prezzoBiglietto;
-//     if (etaPasseggero <= 17) { 
-//         console.log(prezzoBiglietto = prezzoScontoJunior + "€")
-//     } else if (etaPasseggero > 65) { 
-//         console.log(prezzoBiglietto = prezzoScontoOver + "€")
-//     } else {
-//         console.log(prezzoBiglietto = prezzoIntero + "€")
-//     }
-// }
 
-// document.getElementById("prezzo-finito").innerHTML= prezzoBiglietto;
-// Partendo dall'esercizio del biglietto, aggiungiamo la parte visiva come da screenshot
-// Creiamo un finto biglietto del treno con:
-// Nome passeggero
-// Codice treno (numero casuale tra 90000 e 100000 escluso)
-// Numero carrozza
-// Prezzo calcolato
-// Categoria selezionata dall'utente
-// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+
